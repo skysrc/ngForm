be9e51acd576028e87b4591b8bf7b15bff6bb976
Sky <skymsdn@hotmail.com>
2023-11-24 00:09:26 +0800

    implement 2 ways data binding using model

--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserSettings } from '../data/user-settings';
 
 @Component({
   selector: 'app-user-settings-form',
   templateUrl: './user-settings-form.component.html',
   styleUrls: ['./user-settings-form.component.css']
 })
-export class UserSettingsFormComponent {
+export class UserSettingsFormComponent implements OnInit {
+
+  userSettings: UserSettings = {
+    name: 'Milton',
+    emailOffer: true,
+    interfaceStyle: 'dark',
+    subscriptionType: 'Annual',
+    notes: 'here are some notes...'
+  }
+
+  constructor() { }
+
+  ngOnInit() { }
 
 }
