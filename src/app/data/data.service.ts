589d9333f92fb5f8fc853d3586cd0c8d445ceb1e
Sky <skymsdn@hotmail.com>
2023-12-17 11:55:58 +0800

    posting data using observables

--- src/app/data/data.service.ts
+++ src/app/data/data.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { UserSettings } from './user-settings';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -8,7 +9,7 @@ export class DataService {
 
   constructor() { }
 
-  postUserSettingsForm(userSettings: UserSettings) {
-
+  postUserSettingsForm(userSettings: UserSettings): Observable<UserSettings> {
+    return of(userSettings);
   }
 }
--- src/app/user-settings-form/user-settings-form.component.ts
+++ src/app/user-settings-form/user-settings-form.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserSettings } from '../data/user-settings';
 import { NgForm, NgModel } from '@angular/forms';
+import { DataService } from '../data/data.service';
 
 @Component({
   selector: 'app-user-settings-form',
@@ -19,7 +20,7 @@ export class UserSettingsFormComponent implements OnInit {
 
   userSettings: UserSettings = { ...this.originalUserSettings };
 
-  constructor() { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() { }
 
@@ -28,6 +29,10 @@ export class UserSettingsFormComponent implements OnInit {
   }
   onSubmit(form: NgForm) {
     console.log('in submit ', form.valid);
+    this.dataService.postUserSettingsForm(this.userSettings).subscribe(
+      result => console.log('success', result),
+      error => console.log('error: ', error)
+    );
   }
 
 }
